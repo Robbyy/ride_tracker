f85fcf8957f826eec3b4f0e21c6c6836c2cb0006
Robbyy <corbatto.roberto@gmail.com>
2022-03-07 01:31:54 +0100

    SimpleJdbcInsert
    Utilizzo della classe SimpleJdbcInsert per l'inserimento di un record in tabella.

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -29,7 +32,23 @@ public class RideRepositoryImpl implements RideRepository
 	@Override
 	public Ride createRide(Ride ride)
 	{
-		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());	
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+		simpleJdbcInsert.setTableName("ride");
+		
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+		simpleJdbcInsert.setColumnNames(columns);	
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", ride.getName());
+		data.put("duration", ride.getDuration());
+		
+		simpleJdbcInsert.setGeneratedKeyName("id");
+		
+		Number key = simpleJdbcInsert.executeAndReturnKey(data);
+		
+		System.out.println(key);
 		return null;
 	}
 	
