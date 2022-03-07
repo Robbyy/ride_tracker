817789ccda44f73798492dc071a75d9861d3ee24
Robbyy <corbatto.roberto@gmail.com>
2022-03-07 15:50:51 +0100

    PreparedStatement
    Utilizzo della sintassi JdbcTemplate.update() con la sintassi contenente i PreparedStatement.

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -19,8 +19,8 @@ public class RestControllerTest
 		RestTemplate restTemplate = new RestTemplate();
 	
 		Ride ride = new Ride();
-		ride.setName("Bobsled Trail Ride");
-		ride.setDuration(35);
+		ride.setName("Sagebrush Trail");
+		ride.setDuration(33);
 		
 //		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
 	
